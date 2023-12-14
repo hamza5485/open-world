c77b364b6aa4e21634828dfdf292fa016273be40
hamza5485 <hamza5485@gmail.com>
2023-12-14 16:59:12 +1100

    init project structure
    place css in src/assets, add vite-env.d.ts, add app.ts, install node types and update scripts and base config for prod and dev env

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,6 @@
-import {
-  Engine,
-  FreeCamera,
-  HemisphericLight,
-  Mesh,
-  Scene,
-  Vector3,
-} from 'babylonjs';
+import { App } from './app';
 
-const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-const engine = new Engine(canvas, true);
-
-const createScene = () => {
-  const scene = new Scene(engine);
-  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
-  camera.setTarget(Vector3.Zero());
-  camera.attachControl(canvas, false);
-  new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
-  const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene);
-  sphere.position.y = 1;
-  Mesh.CreateGround('ground1', 6, 6, 2, scene);
-
-  return scene;
-};
-const scene = createScene();
-engine.runRenderLoop(() => {
-  scene.render();
-});
-window.addEventListener('resize', () => {
-  engine.resize();
+window.addEventListener('DOMContentLoaded', () => {
+  const app = new App('renderCanvas');
+  app.run();
 });
